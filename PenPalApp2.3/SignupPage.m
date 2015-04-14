29d74d6f93b57181d84db377c486cac8ea71346f
Sebastian Bruce <MacBook@wireless-10-146-138-103.public.utexas.edu>
2015-04-14 13:50:55 -0500

    birthdate

--- PenPalApp2.3/SignupPage.m
+++ PenPalApp2.3/SignupPage.m
@@ -46,6 +46,7 @@
     nameTextField.keyboardType = UIKeyboardTypeDefault;
     nameTextField.clearButtonMode = UITextFieldViewModeWhileEditing;
 #pragma mark - may need to center do diff screen sizes
+    nameTextField.tag = 0;
     nameTextField.delegate = self;
     [scrollView addSubview:nameTextField];
     
@@ -59,6 +60,7 @@
     emailTextField.autocapitalizationType = false;
     emailTextField.keyboardType = UIKeyboardTypeEmailAddress;
     emailTextField.clearButtonMode = UITextFieldViewModeWhileEditing;
+    emailTextField.tag = 1;
     emailTextField.delegate = self;
     [scrollView addSubview:emailTextField];
     
@@ -71,6 +73,7 @@
     passwordTextField.keyboardType = UIKeyboardTypeDefault;
     passwordTextField.clearButtonMode = UITextFieldViewModeWhileEditing;
     passwordTextField.secureTextEntry = true;
+    passwordTextField.tag = 2;
     passwordTextField.delegate = self;
     [scrollView addSubview:passwordTextField];
     
@@ -84,6 +87,7 @@
     usernameTextField.autocapitalizationType = false;
     usernameTextField.keyboardType = UIKeyboardTypeDefault;
     usernameTextField.clearButtonMode = UITextFieldViewModeWhileEditing;
+    usernameTextField.tag = 3;
     usernameTextField.delegate = self;
     [scrollView addSubview:usernameTextField];
 
@@ -91,12 +95,13 @@
     birthTextField.borderStyle = UITextBorderStyleRoundedRect;
     birthTextField.textColor = [UIColor blackColor];
     birthTextField.font = [UIFont systemFontOfSize:17.0];
-    birthTextField.placeholder = @"Birthdate MMDDYYYY";
+    birthTextField.placeholder = @"Birthdate MM-DD-YYYY";
 #pragma mark - may need to auto add / and pad 0's for 1-9.
     birthTextField.backgroundColor = [UIColor clearColor];
     birthTextField.autocorrectionType = UITextAutocorrectionTypeNo;
     birthTextField.keyboardType = UIKeyboardTypeNumberPad;
     birthTextField.clearButtonMode = UITextFieldViewModeWhileEditing;
+    birthTextField.tag = 4;
     birthTextField.delegate = self;
     [scrollView addSubview:birthTextField];
     
@@ -203,6 +208,93 @@
     
     SignupOptions *signupOptions = (SignupOptions *) [self.storyboard instantiateViewControllerWithIdentifier:@"signupOptions"];
     [self presentViewController:signupOptions animated:YES completion:nil];
+}
+
+
+
+NSMutableString *filteredPhoneStringFromStringWithFilter(NSString *string, NSString *filter)
+{
+    NSUInteger onOriginal = 0, onFilter = 0, onOutput = 0;
+    char outputString[([filter length])];
+    BOOL done = NO;
+    
+    while(onFilter < [filter length] && !done)
+    {
+        char filterChar = [filter characterAtIndex:onFilter];
+        char originalChar = onOriginal >= string.length ? '\0' : [string characterAtIndex:onOriginal];
+        switch (filterChar) {
+            case '#':
+                if(originalChar=='\0')
+                {
+                    // We have no more input numbers for the filter.  We're done.
+                    done = YES;
+                    break;
+                }
+                if(isdigit(originalChar))
+                {
+                    outputString[onOutput] = originalChar;
+                    onOriginal++;
+                    onFilter++;
+                    onOutput++;
+                }
+                else
+                {
+                    onOriginal++;
+                }
+                break;
+            default:
+                // Any other character will automatically be inserted for the user as they type (spaces, - etc..) or deleted as they delete if there are more numbers to come.
+                outputString[onOutput] = filterChar;
+                onOutput++;
+                onFilter++;
+                if(originalChar == filterChar)
+                    onOriginal++;
+                break;
+        }
+    }
+    outputString[onOutput] = '\0'; // Cap the output string
+    return [NSMutableString stringWithUTF8String:outputString];
+}
+
+- (BOOL)textField:(UITextField *)textField shouldChangeCharactersInRange:(NSRange)range replacementString:(NSString *)string {
+    
+    NSString *filter = nil;
+    
+    switch(textField.tag) {
+        case 4:
+        {
+            filter = @"##-##-####";
+        }
+        default:
+            break;
+    }
+            
+    if(!filter) return YES; // No filter provided, allow anything
+    
+    NSString *changedString = [textField.text stringByReplacingCharactersInRange:range withString:string];
+    
+    if(range.length == 1 && // Only do for single deletes
+       string.length < range.length &&
+       [[textField.text substringWithRange:range] rangeOfCharacterFromSet:[NSCharacterSet characterSetWithCharactersInString:@"0123456789"]].location == NSNotFound)
+    {
+        // Something was deleted.  Delete past the previous number
+        NSInteger location = changedString.length-1;
+        if(location > 0)
+        {
+            for(; location > 0; location--)
+            {
+                if(isdigit([changedString characterAtIndex:location]))
+                {
+                    break;
+                }
+            }
+            changedString = [changedString substringToIndex:location];
+        }
+    }
+    
+    textField.text = filteredPhoneStringFromStringWithFilter(changedString, filter);
+    
+    return NO;
 }
 
 /*
